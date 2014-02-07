3c98c417f0a9091c3c19cadb3aeea1351212a533
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-07 11:17:58 -0600

    Guess
    Guessing game with do while loop

---
+++ php/guess.php
@@ -0,0 +1,27 @@
+<?php 
+
+$num = mt_rand(1,100);
+$try = 0;
+
+
+do {
+
+fwrite(STDOUT, 'What is your guess? ');
+$guess = fgets(STDIN);
+//fwrite(STDOUT, $guess);
+
+if ($guess > $num) {
+	echo "Lower ";
+}	elseif ($guess < $num) {
+	echo "Higher ";
+}	elseif ($guess == $num) {
+	echo "Good Guess!";
+}
+
+echo "Number of Tries: " . $try++ ;
+
+} while ($guess != $num);
+
+echo "\nGood Game!";
+
+?>
