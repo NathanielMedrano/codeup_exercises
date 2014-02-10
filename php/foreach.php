a46a7c0c6a781c84fb6368c328cad2159adf9a10
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-10 10:44:16 -0600

    foreach
    define types

---
+++ php/foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $thing) {
+	if (is_scalar($thing)) {
+		echo "{$thing} is not NULL\n";
+	}	else {
+		echo "NULL";
+	}
+}
