b42b232c69ddd285030e2b81e68805b0a4f4bab0
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-10 13:21:04 -0600

    foreach
    loops book names

--- php/foreach.php
+++ php/foreach.php
@@ -5,7 +5,5 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 foreach ($things as $thing) {
 	if (is_scalar($thing)) {
 		echo "{$thing} is not NULL\n";
-	}	else {
-		echo "NULL";
-	}
+	}	
 }
