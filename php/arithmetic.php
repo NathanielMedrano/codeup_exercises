92403359e4cccf05d4c3db5f1b346291cdcee8f0
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-11 10:58:58 -0600

    math functions

---
+++ php/arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b) {
+    echo ($a + $b) . "\n";
+}
+
+function sub($a, $b) {
+   echo ($a - $b) . "\n";
+}
+
+function multi($a, $b) {
+    echo ($a * $b) . "\n";
+}
+
+function divide($a, $b) {
+    echo ($a / $b) . "\n";
+}
+
+add(9,8);
+
+sub(9,8);
+
+multi(9,8);
+
+divide(9,8);
+
+?>
