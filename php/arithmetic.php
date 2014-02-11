d12ef1d05136f7fe0cdf89cbc714e403c1699c9c
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-11 13:02:28 -0600

    Validate all the arguments

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,19 +1,43 @@
 <?php
 
 function add($a, $b) {
+    if (is_numeric($a) && is_numeric($b)){
     echo ($a + $b) . "\n";
+	} else {
+		echo "Both must be numbers";
+	}
 }
 
 function sub($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
    echo ($a - $b) . "\n";
+	} else {
+		echo "Both must be numbers";
+	}
 }
 
 function multi($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
     echo ($a * $b) . "\n";
+	} else {
+		echo "Both must be numbers";
+	}
 }
 
 function divide($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
     echo ($a / $b) . "\n";
+	} else {
+		echo "Both must be numbers";
+	}
+}
+
+function mod($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+    echo ($a % $b) . "\n";
+	} else {
+		echo "Both must be numbers";
+	}
 }
 
 add(9,8);
@@ -23,5 +47,7 @@ sub(9,8);
 multi(9,8);
 
 divide(9,8);
+
+mod(9,8);
 
 ?>
